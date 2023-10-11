e66c6e435384a4f90cbd32c73c014a1eda241c69
Thien Le <lbthien5@gmail.com>
2023-10-11 14:45:15 +0700

    Add form thuc an

--- ProjectRetaurantManagement/FormThucAn.Designer.cs
+++ ProjectRetaurantManagement/FormThucAn.Designer.cs
@@ -28,29 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonSuaMonAn = new System.Windows.Forms.Button();
+            this.buttonXoaMonAn = new System.Windows.Forms.Button();
+            this.buttonThemMonAn = new System.Windows.Forms.Button();
+            this.dataGridViewMonAn = new System.Windows.Forms.DataGridView();
+            this.textBoxMaLoaiMonAn = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDonGia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTenMonAn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMaMonAn = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMonAn)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonSuaMonAn
+            // 
+            this.buttonSuaMonAn.Location = new System.Drawing.Point(228, 415);
+            this.buttonSuaMonAn.Name = "buttonSuaMonAn";
+            this.buttonSuaMonAn.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuaMonAn.TabIndex = 4;
+            this.buttonSuaMonAn.Text = "Sửa";
+            this.buttonSuaMonAn.UseVisualStyleBackColor = true;
+            this.buttonSuaMonAn.Click += new System.EventHandler(this.buttonSuaMonAn_Click);
+            // 
+            // buttonXoaMonAn
+            // 
+            this.buttonXoaMonAn.Location = new System.Drawing.Point(130, 415);
+            this.buttonXoaMonAn.Name = "buttonXoaMonAn";
+            this.buttonXoaMonAn.Size = new System.Drawing.Size(75, 23);
+            this.buttonXoaMonAn.TabIndex = 5;
+            this.buttonXoaMonAn.Text = "Xóa";
+            this.buttonXoaMonAn.UseVisualStyleBackColor = true;
+            this.buttonXoaMonAn.Click += new System.EventHandler(this.buttonXoaMonAn_Click);
+            // 
+            // buttonThemMonAn
+            // 
+            this.buttonThemMonAn.Location = new System.Drawing.Point(26, 415);
+            this.buttonThemMonAn.Name = "buttonThemMonAn";
+            this.buttonThemMonAn.Size = new System.Drawing.Size(75, 23);
+            this.buttonThemMonAn.TabIndex = 6;
+            this.buttonThemMonAn.Text = "Thêm";
+            this.buttonThemMonAn.UseVisualStyleBackColor = true;
+            this.buttonThemMonAn.Click += new System.EventHandler(this.buttonThemMonAn_Click);
+            // 
+            // dataGridViewMonAn
+            // 
+            this.dataGridViewMonAn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMonAn.Location = new System.Drawing.Point(-3, 12);
+            this.dataGridViewMonAn.Name = "dataGridViewMonAn";
+            this.dataGridViewMonAn.Size = new System.Drawing.Size(392, 397);
+            this.dataGridViewMonAn.TabIndex = 7;
+            // 
+            // textBoxMaLoaiMonAn
+            // 
+            this.textBoxMaLoaiMonAn.Location = new System.Drawing.Point(547, 230);
+            this.textBoxMaLoaiMonAn.Name = "textBoxMaLoaiMonAn";
+            this.textBoxMaLoaiMonAn.Size = new System.Drawing.Size(222, 20);
+            this.textBoxMaLoaiMonAn.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(440, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Mã loại món ăn";
+            // 
+            // textBoxDonGia
+            // 
+            this.textBoxDonGia.Location = new System.Drawing.Point(547, 174);
+            this.textBoxDonGia.Name = "textBoxDonGia";
+            this.textBoxDonGia.Size = new System.Drawing.Size(222, 20);
+            this.textBoxDonGia.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(440, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Đơn giá";
+            // 
+            // textBoxTenMonAn
+            // 
+            this.textBoxTenMonAn.Location = new System.Drawing.Point(547, 123);
+            this.textBoxTenMonAn.Name = "textBoxTenMonAn";
+            this.textBoxTenMonAn.Size = new System.Drawing.Size(222, 20);
+            this.textBoxTenMonAn.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(440, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tên món ăn";
+            // 
+            // textBoxMaMonAn
+            // 
+            this.textBoxMaMonAn.Location = new System.Drawing.Point(547, 71);
+            this.textBoxMaMonAn.Name = "textBoxMaMonAn";
+            this.textBoxMaMonAn.Size = new System.Drawing.Size(222, 20);
+            this.textBoxMaMonAn.TabIndex = 15;
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(275, 382);
+            this.label1.Location = new System.Drawing.Point(440, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Form";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Mã món ăn";
             // 
             // FormThucAn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxMaLoaiMonAn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDonGia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTenMonAn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMaMonAn);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewMonAn);
+            this.Controls.Add(this.buttonSuaMonAn);
+            this.Controls.Add(this.buttonXoaMonAn);
+            this.Controls.Add(this.buttonThemMonAn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormThucAn";
             this.Text = "FormThucAn";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMonAn)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +173,17 @@
 
         #endregion
 
+        private System.Windows.Forms.Button buttonSuaMonAn;
+        private System.Windows.Forms.Button buttonXoaMonAn;
+        private System.Windows.Forms.Button buttonThemMonAn;
+        private System.Windows.Forms.DataGridView dataGridViewMonAn;
+        private System.Windows.Forms.TextBox textBoxMaLoaiMonAn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDonGia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTenMonAn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMaMonAn;
         private System.Windows.Forms.Label label1;
     }
 }
--- ProjectRetaurantManagement/FormThucAn.cs
+++ ProjectRetaurantManagement/FormThucAn.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ProjectRetaurantManagement
 {
     public partial class FormThucAn : Form
     {
+        QuanLyNhaHangEntities db = new QuanLyNhaHangEntities();
         public FormThucAn()
         {
             InitializeComponent();
+            loadData();
+            AddBinding();
         }
+        void loadData()
+        {
+            var query = from m in db.MonAn
+                        select new
+                        {
+                            MaMonAn = m.MaMonAn,
+                            TenMonAn = m.TenMonAn,
+                            DonGia = m.DonGia,
+                            MaLoaiMonAn = m.MaLoaiMonAn
+                        };
+            dataGridViewMonAn.DataSource = db.MonAn.ToList();
+        }
+        void AddBinding()
+        {
+            textBoxMaMonAn.DataBindings.Add(new Binding("Text", dataGridViewMonAn.DataSource, "MaMonAn"));
+            textBoxTenMonAn.DataBindings.Add(new Binding("Text", dataGridViewMonAn.DataSource, "TenMonAn"));
+            textBoxDonGia.DataBindings.Add(new Binding("Text", dataGridViewMonAn.DataSource, "DonGia"));
+            textBoxMaLoaiMonAn.DataBindings.Add(new Binding("Text", dataGridViewMonAn.DataSource, "MaLoaiMonAn"));
+        }
+
+        private void buttonThemMonAn_Click(object sender, EventArgs e)
+        {
+            MonAn m = new MonAn()
+            {
+                MaMonAn = textBoxMaMonAn.Text,
+                TenMonAn = textBoxTenMonAn.Text,
+                DonGia = int.Parse(textBoxDonGia.Text),
+                MaLoaiMonAn = textBoxMaLoaiMonAn.Text,
+            };
+            db.MonAn.Add(m);
+            db.SaveChanges();
+            loadData();
+        }
+
+        private void buttonXoaMonAn_Click(object sender, EventArgs e)
+        {
+            string maMonAn = textBoxMaMonAn.Text;
+            MonAn m = db.MonAn.Where(p => p.MaMonAn == maMonAn).SingleOrDefault();
+            db.MonAn.Remove(m);
+            db.SaveChanges();
+            loadData();
+        }
+
+        private void buttonSuaMonAn_Click(object sender, EventArgs e)
+        {
+            string maMonAn = dataGridViewMonAn.SelectedCells[0].OwningRow.Cells["MaMonAn"].Value.ToString();
+            MonAn m = db.MonAn.Find(maMonAn);
+            m.MaMonAn = textBoxMaMonAn.Text;
+            m.TenMonAn = textBoxTenMonAn.Text;
+            m.DonGia = int.Parse(textBoxDonGia.Text);
+            m.MaLoaiMonAn = textBoxMaLoaiMonAn.Text;
+            db.SaveChanges();
+            loadData();
+        }
+        
     }
 }
